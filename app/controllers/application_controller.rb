dbfe994db67363ec8a2afe51693f5d57600aeaf3
DanL12186 <DanL12186@users.noreply.github.com>
2017-04-13 16:51:44 -0400

    added current_user/logged_in? helpers
    May modify later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,20 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-configure do
-  set :public_folder, 'public'
-  set :views, 'app/views'
-  enable :sessions
-  set :session_secret, "stealthispasswordsecret"
-end
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "stealthispasswordsecret"
+  end
 
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
 end
