6f581e5ab1bee05607027af923cee5d685d507eb
DanL12186 <DanL12186@users.noreply.github.com>
2017-04-14 15:02:42 -0400

    added check for lowercase as well.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,9 +26,9 @@ class ApplicationController < Sinatra::Base
       params.any? {|param| param[1].empty?}
     end
 
-    def invalid_password?
+    def invalid_password? #ensures user entered an alphanumeric mixed-case password with a symbol. If this returns true, page will reload without explanation.
       password = params[:password]
-      password.length < 10 || !password.include?(/[A-Z]/) || !password.include?(/[\d]/) || !password.include?(/[~!()}\$@\#{%^&*]/)
+      password.length < 10 || !password.include?(/[A-Z]/) || !password.include?(/[a-z]/) !password.include?(/[\d]/) || !password.include?(/[~!()}\$@\#{%^&*]/)
     end
 
     def existing_username?
