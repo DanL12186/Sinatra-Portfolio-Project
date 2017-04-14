125f06d61352d1e5f809db3cb9b1cf6b411f7463
DanL12186 <DanL12186@users.noreply.github.com>
2017-04-14 19:04:38 -0400

    forgot to include require 'open-uri'

--- app/models/bookscrape.rb
+++ app/models/bookscrape.rb
@@ -1,18 +1,20 @@
 class BookScraper
+  require 'open-uri'
+  
+  @@all = []
 
-@@all = []
+  def self.all
+    @@all
+  end
 
-def self.all
-  @@all
-end
+  def self.scrape
+    @@all unless @@all.empty?
 
-def self.scrape
-  @@all unless @@all.empty?
+    page = Nokogiri::HTML(open('http://www.alistofbooks.com/'))
 
-  page = Nokogiri::HTML(open('http://www.alistofbooks.com/'))
-
-  page.css('ul.book-list li').each do |book|
-    @@all << book.css('a.book-title').text.strip
+    page.css('ul.book-list li').each do |book|
+      @@all << book.css('a.book-title').text.strip
     end
   end
 end
+
