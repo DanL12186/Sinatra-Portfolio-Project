37ffc50a7deab7f18d7849f1687c1147b26ffe98
DanL12186 <DanL12186@users.noreply.github.com>
2017-04-14 14:10:36 -0400

    Added (probably) functional login handler

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
     erb :"/users/new_user"
   end
 
+  post '/login' do #handles login submission form. Rejects empty or unauthorized password/username combos.
+    redirect '/login' if required_field_empty?
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id if @user.authenticate(params[:password])
+      redirect "/users/:slug"
+    else
+      redirect '/login'
+    end
+  end
+
 end
