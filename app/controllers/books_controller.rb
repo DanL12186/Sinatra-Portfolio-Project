f9b298688a8d0475cd87410ed689b0117f0075b3
DanL12186 <DanL12186@users.noreply.github.com>
2017-04-15 09:58:52 -0400

    Created routes for book creation
    Also handled persisting new books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,11 @@
 class BooksController < ApplicationController
+
+  get '/new_book' do
+    erb :'/books/new'
+  end
+
+  post '/books' do
+    @book = Book.create(author: params[:author], title: params[:title], user_id: current_user.id)
+    redirect "/users/#{User.find(current_user.id).slug}" #didn't really feel like having to create @user just to set user_id
+  end
 end
