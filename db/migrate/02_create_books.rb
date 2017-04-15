082bba7cf17f7f1b6217900c312318b3fc7af13e
DanL12186 <DanL12186@users.noreply.github.com>
2017-04-14 22:50:28 -0400

    change author to title, drop characters

--- db/migrate/02_create_books.rb
+++ db/migrate/02_create_books.rb
@@ -1,9 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[5.0]
   def change
     create_table :books do |t|
-      t.string :name
+      t.string :title
       t.string :author
-      t.string :characters
       t.integer :user_id
     end
   end
