fe57f1ac7b34e684393bf0616a94826af628201a
DanL12186 <DanL12186@users.noreply.github.com>
2017-04-15 14:20:39 -0400

    handles getting new char pg and new char creation

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,10 @@
 class CharactersController < ApplicationController
+
+  get '/new_character' do
+    logged_in? ? (erb :'/characters/new') : (redirect "/login")
+  end
+
+  post '/characters' do #handles char creation
+    Character.create(params) #both creates and associates char w/book through id (name="book_id"). Adds any new character added to char array.
+  end
 end
