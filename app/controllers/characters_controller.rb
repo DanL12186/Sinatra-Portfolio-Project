cc669a89d37bd20c6c9b213bfdbdc48b306eb57d
DanL12186 <DanL12186@users.noreply.github.com>
2017-04-15 20:07:46 -0400

    added redirects and character show route

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,6 +5,13 @@ class CharactersController < ApplicationController
   end
 
   post '/characters' do #handles char creation
+    redirect '/new_character' if required_field_empty?
     Character.create(params) #both creates and associates char w/book through id (name="book_id"). Adds any new character added to char array.
+    redirect '/characters/show'
   end
+
+  get '/characters/show' do #points to character show page which displays all characters a user has across all his/her books.
+    erb :'/characters/show'
+  end
+
 end
