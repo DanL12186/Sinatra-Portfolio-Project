e096633502ef90cb32926cfb57fd431a19bfb271
DanL12186 <danlrcn@hotmail.com>
2018-04-27 10:52:00 -0400

    updated files and fixed bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
     def logged_in?
       !!session[:user_id]
     end
-    
+
     def redirect_if_not_logged_in
       if !logged_in?
         redirect "/login?error=You have to be logged in to do that"
@@ -30,12 +30,12 @@ class ApplicationController < Sinatra::Base
     end
 
     def required_field_empty?
-      params.any? {|param| param[1].empty?} #this can get snagged on an empty splat.
+      params.any? { |param| param[1].empty? } #this can get snagged on an empty splat.
     end
 
     def invalid_password? #ensures user entered an alphanumeric mixed-case pwd with a symbol. If this returns true, page will reload without explanation.
       pwd = params[:password] #password is in plaintext at this juncture...
-      pwd.length < 10 || !pwd.match(/[A-Z]/) || !pwd.match(/[a-z]/) || !pwd.match(/[\d]/) || !pwd.match(/[~!()}\$@\#{%^&*]/)
+      pwd.length < 10 || !pwd.match(/[A-Z]/) || !pwd.match(/[a-z]/) || !pwd.match(/\d/) || !pwd.match(/[~!()}\$@\#{%^&*]/)
     end
 
     def existing_username?
